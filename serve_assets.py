055f8417ddfeabac18f6bc2142b19d5ce291ee01
aadi9801 <aadityakumar8878@gmail.com>
2026-01-14 13:57:59 +0530

    Add files via upload

---
+++ serve_assets.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Simple HTTP server with CORS enabled for skillmap assets.
+This allows the dashboard to dynamically fetch video files.
+"""
+
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        # Enable CORS for all requests
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        super().end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.end_headers()
+
+if __name__ == '__main__':
+    port = 8002
+    server_address = ('', port)
+    
+    # Change to skillmap directory
+    os.chdir('../skillmap')
+    
+    httpd = HTTPServer(server_address, CORSRequestHandler)
+    print(f'Starting CORS-enabled server on port {port}...')
+    print(f'Serving directory: {os.getcwd()}')
+    print(f'Assets available at: http://localhost:{port}/assets/')
+    httpd.serve_forever()
